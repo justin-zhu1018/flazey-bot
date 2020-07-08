fe93a2fbf99b617db35eebd242449c2ce33eb407
unknown <justin.zhu@sjsu.edu>
2020-07-08 13:50:22 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("Connected as " + client.user.tag);
+  client.user.setActivity("with Javascript");
+
+  client.guilds.cache.forEach((guild) => {
+    console.log(guild.name);
+    // guild.channels.cache.forEach((channel) => {
+    //   console.log(` - ${channel.name} ${channel.type} ${channel.id}`);
+    // });
+  });
+  let generalChannel = client.channels.cache.get("730511965092446361");
+  const attachment = new Discord.MessageAttachment(
+    "https://clashroyale.com/uploaded-images-blog/CR_facebook_share_02_180403_175322.jpg?mtime=20180403175322"
+  );
+  generalChannel.send("Hello World!", attachment);
+});
+
+client.on("message", (receivedMessage) => {
+  if (receivedMessage.author == client.user) {
+    return;
+  }
+  if (receivedMessage.content.startsWith("!")) {
+    processCommand(receivedMessage);
+  }
+});
+
+function processCommand(receivedMessage) {
+  let command = receivedMessage.content.substr(1);
+  if (command === "help") {
+    receivedMessage.channel.send("No you're not helping");
+  }
+  //   receivedMessage.channel.send("Yup", receivedMessage.content);
+}
+
+client.login("NzA4ODA1NTQ4MDM1ODAxMjY5.XwYkvA.DgEG4CMdqTzgYZoPWjKIUDllCOE");
