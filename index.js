362fa37ae3f508cfd858f21de5df4f21abb1830b
unknown <justin.zhu@sjsu.edu>
2020-07-08 14:55:57 -0700

    remove config.json function call

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+// const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
