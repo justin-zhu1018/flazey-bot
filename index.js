dd491298d818d436049ae240bb977b6392e7067d
unknown <justin.zhu@sjsu.edu>
2020-07-08 21:40:16 -0700

    api test

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const Client = require("clash-royale-api");
 // console.log("Test1 ", process.env.Test1);
 // console.log("Test2 ", process.env.Test2);
 // console.log("Test3 ", process.env.Test3);
+// const fetch = require("node-fetch");
 
 //Used in testing
-const config = require("./config.json");
-const clash = new Client(config.testToken);
+// const config = require("./config.json");
+// const clash = new Client(config.testToken);
 
-// const clash = new Client(config.tokenCR);
-// const fetch = require("node-fetch");
+//Used for Heroku
+const clash = new Client(config.tokenCR);
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
@@ -23,13 +24,21 @@ client.on("ready", () => {
       console.log(` - ${channel.name} ${channel.type} ${channel.id}`);
     });
   });
-  let generalChannel = client.channels.cache.get("730295406189215747");
-  // let botChannel = client.channels.cache.get("730528385385889834");
+  // let generalChannel = client.channels.cache.get("730295406189215747");
+  let botChannel = client.channels.cache.get("730528385385889834");
   //   const attachment = new Discord.MessageAttachment(
   //     "https://clashroyale.com/uploaded-images-blog/CR_facebook_share_02_180403_175322.jpg?mtime=20180403175322"
   //   );
 
   // generalChannel.send("I am here! For a list of the commands, type !commands");
+
+  // botChannel.send(
+  //   "I am here! List of commands: ```" +
+  //     "!commands\n" +
+  //     "!help\n" +
+  //     "!clan\n" +
+  //     "!player [player-tag] ```"
+  // );
 });
 
 client.on("message", (receivedMessage) => {
@@ -43,14 +52,20 @@ client.on("message", (receivedMessage) => {
 
 function processCommand(receivedMessage) {
   let fullCommand = receivedMessage.content.substr(1);
-  let splitCommand = command.split(" ");
+  let splitCommand = fullCommand.split(" ");
   let primaryCommand = splitCommand[0];
   let secondaryCommand = splitCommand[1];
 
   const channel = receivedMessage.channel;
 
   if (primaryCommand === "commands") {
-    channel.send("Here's a list of commands: !help, !clan, !player");
+    channel.send(
+      "List of commands: ```" +
+        "!commands\n" +
+        "!help\n" +
+        "!clan\n" +
+        "!player [player-tag] ```"
+    );
   } else if (primaryCommand === "help") {
     channel.send("You don't need help shhh...");
   } else if (primaryCommand === "clan") {
@@ -70,18 +85,71 @@ async function processClan(channel) {
   const clan = await clash.clan("#YQYYGC02");
   console.log("clan: ", clan.data.name, clan.data.tag, clan.data.description);
   channel.send(
-    "Our clan: \n" +
+    "```Our clan: \n" +
       clan.data.name +
-      ", " +
+      "\nTag: " +
       clan.data.tag +
-      ", " +
-      clan.data.description
+      "\nDescription: " +
+      clan.data.description +
+      "```"
   );
 }
 
 async function processPlayer(secondaryCommand, channel) {
-  const player = await clash.player(secondaryCommand);
-  channel.send("Player: ", player);
+  if (secondaryCommand === undefined) {
+    channel.send("Invalid player tag, try again!");
+  } else if (secondaryCommand.length !== 9) {
+    channel.send("Invalid player tag, try again!");
+  } else {
+    // const player = await clash.player(secondaryCommand);
+    const player = await clash.player(secondaryCommand.toUpperCase());
+    const img = player.data.currentFavouriteCard.iconUrls.medium;
+    // const attachment = new Discord.MessageAttachment(img);
+    // console.log("Secondary Command: ", secondaryCommand);
+    // console.log("Player data: ", player.data);
+    console.log("Card: ", img);
+    // channel.send(
+    //   "Player " +
+    //     player.data.name +
+    //     ": \n" +
+    //     player.data.trophies +
+    //     " trophies \nLevel " +
+    //     player.data.expLevel
+    // );
+    channel.send(
+      "```Player: " +
+        player.data.name +
+        " \nTrophy Count: " +
+        player.data.trophies +
+        "  \nLevel " +
+        player.data.expLevel +
+        "\nRole: " +
+        player.data.role +
+        "\nWar Day Wins: " +
+        player.data.warDayWins +
+        "\nCurrent Favorite Card: " +
+        player.data.currentFavouriteCard.name +
+        "```"
+    );
+    // channel.send(
+    //   "**Player: " +
+    //     player.data.name +
+    //     "** \nTrophy Count: " +
+    //     player.data.trophies +
+    //     "  \nLevel " +
+    //     player.data.expLevel
+    // );
+  }
+
+  // console.log("Test:)
+  // try {
+  //   const player = await clash.player(secondaryCommand);
+  //   channel.send("Player: "+ player);
+  // } catch (error) {
+  //   channel.send("Player: "+ secondaryCommand+ " is invalid. Please try again.");
+
+  //   console.log(error);
+  // }
 }
 // async function fetchData() {
 //   const response = await fetch(
@@ -113,5 +181,5 @@ async function processPlayer(secondaryCommand, channel) {
 //   }
 // }
 
-// client.login(process.env.BOT_TOKEN);
-client.login(config.token);
+client.login(process.env.BOT_TOKEN);
+// client.login(config.token);
