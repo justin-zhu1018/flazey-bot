1b26a9a0a018359f6212de1f35ae7854ae30a61e
unknown <justin.zhu@sjsu.edu>
2020-07-09 18:17:25 -0700

    Improved command functions, 'cards' command works

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 const Client = require("clash-royale-api");
 
 //Used in testing
-const config = require("./config.json");
-const clash = new Client(config.testToken);
+// const config = require("./config.json");
+// const clash = new Client(config.testToken);
 
 //Used for Heroku
-// const clash = new Client(process.env.CR_API_TOKEN);
+const clash = new Client(process.env.CR_API_TOKEN);
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
@@ -19,13 +19,13 @@ client.on("ready", () => {
       console.log(` - ${channel.name} ${channel.type} ${channel.id}`);
     });
   });
-  // let generalChannel = client.channels.cache.get("730295406189215747");
+  let generalChannel = client.channels.cache.get("730295406189215745");
   let botChannel = client.channels.cache.get("730528385385889834");
   //   const attachment = new Discord.MessageAttachment(
   //     "https://clashroyale.com/uploaded-images-blog/CR_facebook_share_02_180403_175322.jpg?mtime=20180403175322"
   //   );
 
-  // generalChannel.send("I am here! For a list of the commands, type !commands");
+  generalChannel.send("I am here! For a list of the commands, type !commands.");
 
   // botChannel.send(
   //   "I am here! List of commands: ```" +
@@ -54,19 +54,11 @@ function processCommand(receivedMessage) {
   const channel = receivedMessage.channel;
 
   if (primaryCommand === "commands") {
-    channel.send(
-      "List of commands: ```" +
-        "!commands\n" +
-        "!help\n" +
-        "!clan\n" +
-        "!player [player-tag]\n" +
-        "!cards [player-tag]\n" +
-        "```"
-    );
+    processCommandsList(channel);
   } else if (primaryCommand === "help") {
-    channel.send("You don't need help shhh... just use **!commands**");
+    processHelp(channel);
   } else if (primaryCommand === "clan") {
-    processClan(channel);
+    processClan(secondaryCommand, channel);
   } else if (primaryCommand === "player") {
     processPlayer(secondaryCommand, channel);
   } else if (primaryCommand === "cards") {
@@ -78,41 +70,105 @@ function processCommand(receivedMessage) {
   }
 }
 
-async function processClan(channel) {
-  // const res = await fetchData();
-  // const res = await fetchCR();
-  const clan = await clash.clan("#YQYYGC02");
-  console.log("clan: ", clan.data.name, clan.data.tag, clan.data.description);
-  // channel.send(
-  //   "```Our clan: \n" +
-  //     clan.data.name +
-  //     "\nTag: " +
-  //     clan.data.tag +
-  //     "\nDescription: " +
-  //     clan.data.description +
-  //     "```"
-  // );
-
+function processCommandsList(channel) {
   const embed = new Discord.MessageEmbed()
     .setColor("#0099ff")
-    .setTitle(clan.data.name + " " + clan.data.tag)
-    .setURL("https://royaleapi.com/clan/" + clan.data.tag.substr(1))
+    .setTitle("The Great Information Board")
     .setAuthor(
-      "Flazey's Clan Thang",
-      "https://i.ytimg.com/vi/CCYCI9FINME/maxresdefault.jpg"
-      // "https://discord.js.org"
+      "Flazey's Command List Thang",
+      "https://i.ytimg.com/vi/CCYCI9FINME/maxresdefault.jpg",
+      "https://github.com/justin-zhu1018/flazey-bot"
     )
-    .setDescription("Clan Stuff")
-    // .setThumbnail(player.data.currentFavouriteCard.iconUrls.medium)
+    .setDescription(
+      "Here is a list of all resources that you could make use of."
+    )
     .addFields(
-      { name: "Description", value: clan.data.description, inline: false },
-      { name: "Tag", value: clan.data.tag, inline: false }
-      // { name: "Level 11", value: level11, inline: false },
-      // { name: "Level 10", value: level10, inline: false }
+      {
+        name: "!commands",
+        value: "To bring this thing up again.",
+        inline: false,
+      },
+      {
+        name: "!clan [clan-tag] || !clan",
+        value: "Look up a clan!",
+        inline: false,
+      },
+      {
+        name: "!player [player-tag]",
+        value: "Stalk your special someone <3",
+        inline: false,
+      },
+      {
+        name: "!cards [player-tag]",
+        value: "Admire your special someone's cards <33",
+        inline: false,
+      },
+      {
+        name: "!help",
+        value: "For when you need help.",
+        inline: false,
+      }
     )
     .setTimestamp();
 
   channel.send(embed);
+}
+
+function processHelp(channel) {
+  const embed = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Need help?")
+    .setAuthor(
+      "Flazey's Helper Thang",
+      "https://i.ytimg.com/vi/CCYCI9FINME/maxresdefault.jpg"
+      // "https://discord.js.org"
+    )
+    .setThumbnail("https://cdn.frankerfacez.com/emoticon/263833/4")
+    .setDescription(
+      "You don't need help shhh... just use **!commands**. But if you insist... follow this [link](https://www.youtube.com/watch?v=dQw4w9WgXcQ)."
+    )
+    .setTimestamp();
+
+  channel.send(embed);
+}
+
+async function processClan(secondaryCommand, channel) {
+  var clan = undefined;
+  if (secondaryCommand === undefined) {
+    clan = await clash.clan("#YQYYGC02");
+    // console.log("clan: ", clan.data.name, clan.data.tag, clan.data.description);
+  } else {
+    try {
+      if (secondaryCommand.substr(0, 1) !== "#") {
+        var secondaryCommand = "#" + secondaryCommand;
+      }
+      clan = await clash.clan(secondaryCommand.toUpperCase());
+    } catch (error) {
+      channel.send(
+        "Invalid clan tag: **" +
+          secondaryCommand +
+          "**. Try again! (ex: !clan #YQYYGC02 or !clan YQYYGC02)"
+      );
+    }
+  }
+  try {
+    const embed = new Discord.MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle(clan.data.name + " " + clan.data.tag)
+      .setURL("https://royaleapi.com/clan/" + clan.data.tag.substr(1))
+      .setAuthor(
+        "Flazey's Clan Thang",
+        "https://i.ytimg.com/vi/CCYCI9FINME/maxresdefault.jpg"
+      )
+      .setDescription("Clan Stuff")
+      .addFields(
+        { name: "Description", value: clan.data.description, inline: false },
+        { name: "Tag", value: clan.data.tag, inline: false }
+      )
+      .setTimestamp();
+
+    channel.send(embed);
+  } catch (error) {}
 }
 
 async function processPlayer(secondaryCommand, channel) {
@@ -120,10 +176,7 @@ async function processPlayer(secondaryCommand, channel) {
     if (secondaryCommand.substr(0, 1) !== "#") {
       var secondaryCommand = "#" + secondaryCommand;
     }
-    const player = await clash.player(secondaryCommand.toUpperCase());
-    // const img = player.data.currentFavouriteCard.iconUrls.medium;
-
-    // console.log("Card: ", img);
+    var player = await clash.player(secondaryCommand.toUpperCase());
 
     const embed = new Discord.MessageEmbed()
       .setColor("#0099ff")
@@ -132,7 +185,6 @@ async function processPlayer(secondaryCommand, channel) {
       .setAuthor(
         "Flazey's Player Profile Thang",
         "https://i.ytimg.com/vi/CCYCI9FINME/maxresdefault.jpg"
-        // "https://discord.js.org"
       )
       .setDescription("Player Profile ez dab")
       .setThumbnail(player.data.currentFavouriteCard.iconUrls.medium)
@@ -140,19 +192,23 @@ async function processPlayer(secondaryCommand, channel) {
         { name: "Trophy Count", value: player.data.trophies, inline: false },
         { name: "Level", value: player.data.expLevel, inline: false },
         { name: "Role", value: player.data.role, inline: false },
-        { name: "War Day Wins", value: player.data.warDayWins, inline: false }
+        { name: "War Day Wins", value: player.data.warDayWins, inline: false },
+        {
+          name: "Favorite Card: ",
+          value: player.data.currentFavouriteCard.name,
+          inline: false,
+        }
       )
       .setTimestamp();
 
     channel.send(embed);
   } catch (error) {
     channel.send(
-      "Invalid gamer tag: " +
+      "Invalid gamer tag: **" +
         secondaryCommand +
-        ". Try again! (ex: !player #YG200UJ0 or !player YG200UJ0)"
+        "**. Try again! (ex: !player #YG200UJ0 or !player YG200UJ0)"
     );
   }
-  // }
 }
 
 async function processCards(secondaryCommand, channel) {
@@ -160,26 +216,32 @@ async function processCards(secondaryCommand, channel) {
     if (secondaryCommand.substr(0, 1) !== "#") {
       var secondaryCommand = "#" + secondaryCommand;
     }
-    const player = await clash.player(secondaryCommand.toUpperCase());
+
+    var player = await clash.player(secondaryCommand.toUpperCase());
+
+    //Testing
+    // const player = await clash.player("#YG200UJ0");
+
+    // console.log(player.data.cards);
+
     let arr = player.data.cards;
-    let sortedArr = sortByKey(arr, "level");
+    let sortedArr = sortByKey(arr, "level", "maxLevel");
     let level13 = [];
     let level12 = [];
     let level11 = [];
     let level10 = [];
+
     // console.log(sortedArr);
+
     for (i = 0; i < sortedArr.length; i++) {
-      if (sortedArr[i].level === 10) {
-        // console.log("level 10");
+      var level = 13 - (sortedArr[i].maxLevel - sortedArr[i].level);
+      if (level === 10) {
         level10.push(sortedArr[i].name);
-      } else if (sortedArr[i].level === 11) {
-        // console.log("level 11");
+      } else if (level === 11) {
         level11.push(sortedArr[i].name);
-      } else if (sortedArr[i].level === 12) {
-        // console.log("level 12");
+      } else if (level === 12) {
         level12.push(sortedArr[i].name);
-      } else if (sortedArr[i].level === 13) {
-        // console.log("level 13");
+      } else if (level === 13) {
         level13.push(sortedArr[i].name);
       }
     }
@@ -195,6 +257,7 @@ async function processCards(secondaryCommand, channel) {
     if (level10.length === 0) {
       level10.push("N/A");
     }
+
     const embed = new Discord.MessageEmbed()
       .setColor("#0099ff")
       .setTitle(player.data.name + " " + player.data.tag)
@@ -202,7 +265,6 @@ async function processCards(secondaryCommand, channel) {
       .setAuthor(
         "Flazey's Card Thang",
         "https://i.ytimg.com/vi/CCYCI9FINME/maxresdefault.jpg"
-        // "https://discord.js.org"
       )
       .setDescription("Cards")
       .setThumbnail(player.data.currentFavouriteCard.iconUrls.medium)
@@ -217,53 +279,23 @@ async function processCards(secondaryCommand, channel) {
     channel.send(embed);
   } catch (error) {
     channel.send(
-      "Invalid gamer tag: " +
+      "Invalid gamer tag: **" +
         secondaryCommand +
-        ". Try again! (ex: !cards #YG200UJ0 or !cards YG200UJ0)"
+        "**. Try again! (ex: !cards #YG200UJ0 or !cards YG200UJ0)"
     );
-    console.log("error: ", error);
   }
 }
 
-function sortByKey(array, key) {
+function sortByKey(array, key, maxLevel) {
   return array.sort(function (a, b) {
-    var x = a[key];
-    var y = b[key];
+    var x = 13 - (a[maxLevel] - a[key]);
+    var y = 13 - (b[maxLevel] - b[key]);
     return x < y ? -1 : x > y ? 1 : 0;
   });
 }
-// async function fetchData() {
-//   const response = await fetch(
-//     "https://finnhub.io/api/v1/quote?symbol=MSFT&token=br906t7rh5ral083k820"
-//   );
-//   const json = await response.json();
-//   // console.log("json: ", json);
-//   return json;
-// }
-
-// async function fetchCR() {
-//   console.log("try fetch start");
-//   try {
-//     const response = await fetch(
-//       "https://api.clashroyale.com/v1/clans/%23YQYYGC02",
-//       {
-//         method: "get",
-//         // url: ``,
-//         // baseURL: 'https://api.clashroyale.com/v1/',
-//         headers: {
-//           Accept: "application/json",
-//           authorization: `Bearer ${config.tokenCR}`,
-//         },
-//       }
-//     );
-//     console.log("response: ", response.body);
-//   } catch (error) {
-//     console.log("error: ");
-//   }
-// }
 
 //Used in Heroku
-// client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 //Used in testing
-client.login(config.token);
+// client.login(config.token);
